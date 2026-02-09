751924043d38c456f28228ea12649006ae933229
Denis Faini <dfaini25@gmail.com>
2026-02-08 21:16:26 -0500

    refactor git completion setup to use fpath and compinit, removing Homebrew dependency

--- git/completion.zsh
+++ git/completion.zsh
@@ -1,9 +1,3 @@
-# Uses git's autocompletion for inner commands. Assumes an install of git's
-# bash `git-completion` script at $completion below (this is where Homebrew
-# tosses it, at least).
-completion='$(brew --prefix)/share/zsh/site-functions/_git'
-
-if test -f $completion
-then
-  source $completion
-fi
+# git completion is provided via fpath + compinit.
+# Do not source Homebrew's `_git` directly: it is an autoloaded completion
+# function file and executes completion internals only in completion context.
