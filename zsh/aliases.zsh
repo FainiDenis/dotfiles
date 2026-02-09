0e819fae516ba6ce59b689c41bcf928e0bffee16
Denis Faini <dfaini25@gmail.com>
2026-02-08 23:00:28 -0500

    add: enhance zsh aliases for improved networking commands

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,23 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+alias myip='ipconfig getifaddr en0 || ipconfig getifaddr en1'
+alias publicip='curl -s https://ifconfig.me'
+alias pingg='ping -c4 google.com'
+
+ping() { command ping -c4 "$@"; }   # Default to 4 pings instead of infinite
+
+alias dfh='df -h'
+alias ports='lsof -nP -iTCP -sTCP:LISTEN'
+alias brewupdate='command brew update && command brew upgrade && command brew cleanup'
+
+alias myipall='ifconfig | grep "inet "'
+alias gateway='route -n get default | grep gateway'
+alias dnsservers='scutil --dns | grep "nameserver\\[[0-9]*\\]"'
+alias arpall='arp -a'
+alias netlisten='netstat -anv | grep LISTEN'
+alias netest='netstat -anv | grep ESTABLISHED'
+alias tracer='traceroute'
+alias ns='nslookup'
+
+diga() { dig +short A "$1"; }   # Get the A record for a domain
