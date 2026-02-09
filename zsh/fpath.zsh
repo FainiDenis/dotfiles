d2788cef1bc803fe62ca517f82c39b78d92f2a51
Denis Faini <dfaini25@gmail.com>
2026-02-08 21:16:51 -0500

    fix: ensure Homebrew path is added to fpath only if brew is installed

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,2 +1,6 @@
 #add each topic folder to fpath so that they can add functions and completion scripts
 for topic_folder ($ZSH/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+
+if command -v brew >/dev/null 2>&1; then
+  fpath=("$(brew --prefix)/share/zsh/site-functions" $fpath)
+fi
