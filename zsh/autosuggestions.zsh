e1213898c6043277442b0ef5b6517291898f5adf
Denis Faini <dfaini25@gmail.com>
2026-02-08 22:59:45 -0500

    fix: use command substitution for brew prefix

--- zsh/autosuggestions.zsh
+++ zsh/autosuggestions.zsh
@@ -3,7 +3,7 @@ if [[ ! -o interactive ]]; then
 fi
 
 if command -v brew >/dev/null 2>&1; then
-  autosuggestions_path="$(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+  autosuggestions_path="$(command brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
   if [[ -f "$autosuggestions_path" ]]; then
     source "$autosuggestions_path"
   fi
