e8fa610a76222fa0f0b1fbaea2b60fb98c9cfd38
Denis Faini <dfaini25@gmail.com>
2026-02-08 21:16:40 -0500

    add zsh autosuggestions script for improved command line experience

---
+++ zsh/autosuggestions.zsh
@@ -0,0 +1,10 @@
+if [[ ! -o interactive ]]; then
+  return
+fi
+
+if command -v brew >/dev/null 2>&1; then
+  autosuggestions_path="$(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+  if [[ -f "$autosuggestions_path" ]]; then
+    source "$autosuggestions_path"
+  fi
+fi
